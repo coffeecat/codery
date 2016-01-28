b93de8c334012997b40a47bf8963d0bf99bf67fb
Steven Verriest <stevenverriest@gmail.com>
2016-01-28 16:08:02 -0500

    Changes to alpha.php and index on both github and my mac

--- alpha.php
+++ alpha.php
@@ -1,4 +1,4 @@
 abcdefghijklmnopqrstuvwxyz
  hello world - i added this on my computer
---I added this on my mac!!!
+--I added this on my mac!!! - 4:06
  ~I added this on GitHub
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 hello world
 it's hammer time 
 * I added this line from gitHub.com 
-~ I added this line from my mac
+~ I added this line from my mac - 4:07
 !I ADDED THIS ON GITHUB
