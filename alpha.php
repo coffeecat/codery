23d19efd32691537856150f7ddb234ae2ccf3e6a
coffeecat <stevenverriest@gmail.com>
2016-01-29 12:08:22 -0500

    Update alpha.php

--- alpha.php
+++ alpha.php
@@ -3,8 +3,8 @@ On the mac - eleted everything  9:12 - 11:14
 11:41
 this is GitSomenew testHb - 9:13 
 11:40
-whoa NEW 11:45 Lorem NEW ipsum dolor sit amet, consectetur adipiscing elit. Praesent consectetur maximus lacinia. 
-Nulla id posuere mi, ut NEW dapibus quam. Praesent ut commodo nunc. Sed dictum ex vitae sagittis dictum. 
+whoa NOT SO NEW 12:08... Lorem NEW ipsum dolor sit amet, consectetur adipiscing elit. Praesent consectetur maximus lacinia. 
+Nulla id posuere mi, ut super-NEW dapibus quam. Praesent ut commodo nunc. Sed dictum ex vitae sagittis dictum. 
 Praesent at arcu at erat dictum feugiat. Sed aliquet fermentum dolor id pulvinar. Suspendisse potenti. 
 Nullam viverra mi eget nibh egestas, sed laoreet neque volutpat. Morbi mattis quam tortor. 
 In a lorem in tellus pellentesque condimentum eu vel dui. Etiam lacus libero, elementum a nisl eget, 
