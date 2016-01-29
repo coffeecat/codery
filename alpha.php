48a9e41d9767efbcbda03de0093346496c4f501e
Steven Verriest <stevenverriest@gmail.com>
2016-01-29 09:14:56 -0500

    created a conflict to text merge tool

--- alpha.php
+++ alpha.php
@@ -1,5 +1,1 @@
-abcdefghijklmnopqrstuvwxyz
- hello world - i added this on my computer
---I added this on my mac!!! - 4:06
- ~I added this on GitHub
-4:05 add on github
+mac - deleted everything  9:12 - 
