839705087329b36393c9a7b4eb2e258aae73f63d
Steven Verriest <stevenverriest@gmail.com>
2016-01-30 10:26:48 -0500

    simple change to create a conflict

--- alpha.php
+++ alpha.php
@@ -2,14 +2,14 @@ world
 On the mac - eleted everything  9:12 - 11:14
 11:41
 this is GitSomenew testHb - 9:13 
-11:40
-whoa NEW 12:07 Lorem VERY NEW ipsum dolor sit amet, consectetur adipiscing elit. Praesent consectetur maximus lacinia. 
-Nulla NEW 10:19 id posuere mi, ut VERY NEW dapibus quam. Praesent ut commodo nunc. Sed dictum ex vitae sagittis dictum. 
+11:4a 0 LOCAL CHANGE 10:25
+whoa NEW 12:07 Lorem VERY NEW ipsum dolor sit amet, consectetur adipiscing elit.LOCAL CHANGE 10:25 Praesent consectetur maximus lacinia. 
+Nulla NEW 10:19 id posuere mi, ut VERY NEW dapibus quam. PraesLOCAL CHANGE 10:25ent ut commodo nunc. Sed dictum ex vitae sagittis dictum. 
 whoa NOT SO NEW 12:08... Lorem NEW ipsum dolor sit amet, consectetur adipiscing elit. Praesent consectetur maximus lacinia. 
-Nulla id posuereNEW 10:19 mi, ut super-NEW dapibus quam. Praesent ut commodo nunc. Sed dictum ex vitae sagittis dictum. 
+Nulla id posuereNEW 10:19 LOCAL CHANGE 10:25mi, ut super-NEW dapibus quam. Praesent ut commodo nunc. Sed dictum ex vitae sagittis dictum. 
 Praesent at arcu at erat dictum feugiat. Sed aliquet fermentum dolor id pulvinar. Suspendisse potenti. 
 Nullam viverra mi eget nibh egestas, sed laoreet neque volutpat. Morbi mattis quam tortor. 
 In a lorem in tellus pellentesque condimentum eu vel dui. Etiam lacus libero, elementum a nisl eget, 
-tempor aliquet nunc. Vestibulum vel quam condimentum, tempor enim at, mollis mi. Nam vel condimentum nisl. 
+tempor aliquet nunc. Vestibulum vel quam conLOCAL CHANGE 10:25dimentum, tempor enim at, mollis mi. Nam vel condimentum nisl. 
 Phasellus maximus tellus diam, sit amet aliquet massa laoreet vel. Suspendisse placerat id nisi non suscipit.
 test after moving to GitProjects
